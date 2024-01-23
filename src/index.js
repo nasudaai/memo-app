571308a373a820b08930ec15bee340536f85b1e2
nasuda <nasuda908@gmail.com>
2024-01-24 07:25:17 +0900

    add eventListener button

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ console.log('index.js')
 const textElm = document.getElementById('add-text')
 console.log(textElm)
 
-textElm.addEventListener('change', () => {
-  const text = textElm.value
+/*
+textElm.addEventListener('change', (e) => {
+  const text = e.target.value
   console.log(text)
 })
 
@@ -12,3 +13,16 @@ textElm.addEventListener('change', () => {
 textElm.addEventListener('input', (e) => {
   console.log(e.target.value)
 })
+*/
+
+const addButton = document.getElementById('add-button')
+addButton.addEventListener('click', () => {
+  const text = textElm.value
+
+  textElm.value = ''
+  console.log(text)
+})
+
+
+
+
